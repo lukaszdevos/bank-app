a28e1a5479cf8ebbbf66c3a0952fb98f99aaf7a9
lukasz_laszuk <lukasz.laszukdev@gmail.com>
2020-09-27 12:07:05 +0200

    mongodb init

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from instance.config import app_config
+from database.db import initialize_db
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/bankapp"
+    initialize_db(app)
     api = Api(app)
 
     return app
